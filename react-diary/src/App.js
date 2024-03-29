9987ba4d14eb5800ee7abbc27637a37ac431fc96
Jere <jerekoivisto7@gmail.com>
2024-03-29 17:02:01 +0200

    Initial coding. Starting page is done. Started doing animated button

---
+++ react-diary/src/App.js
@@ -0,0 +1,60 @@
+import './App.css';
+import { motion } from 'framer-motion';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import NavBar from 'react-bootstrap/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is my first diary.</h1>
+        <a href="#">
+      <span>Button</span>
+      </a>
+      </header>
+
+
+    </div>
+  );
+}
+
+function AnimatedButton(){
+  const 
+}
+
+export default App;
+
+/* function TopNavBar()
+{
+return (
+  <>
+  <NavBar bg="dark" data-bs-theme="dark"></NavBar>
+  <Container>
+    <NavBar.Brand href="#home"></NavBar.Brand>
+  </Container>
+  </>
+)
+} */
+
+
+
+
+{/* <Box
+component="form"
+sx={{
+  '& .MuiTextField-root': { m: 1, width: '25ch'},
+}}
+noValidate
+autoComplete='off'
+></Box> */}
+
+{/* <TextField
+id="standard-textarea"
+label="Multiline"
+placeholder="Placeholder"
+multiline
+variant="standard"
+></TextField> */}
