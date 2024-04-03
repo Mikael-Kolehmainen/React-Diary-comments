b18ea65cb1229a90a75606b3d7ff2c337d13d4f9
Jere <jerekoivisto7@gmail.com>
2024-04-03 17:42:11 +0300

    Figuring out the transitions

--- src/StartingScreen/StartingScreen.js
+++ src/StartingScreen/StartingScreen.js
@@ -1,14 +1,42 @@
+import React, { useState, useEffect } from 'react';
 import './StartingScreen.css';
+import LoginScreen from '../LoginScreen/LoginScreen.js';
+import { Route, Routes, Link } from "react-router-dom";
 
+const StartingScreen = () => {
+  useEffect(() => {
+    const handleKeyPress = () => {
+      <Routes>
+      <Route path="/Loginscreen" element={<LoginScreen />} />
+      </Routes>
 
-function StartingScreen() {
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
+
+  const handleMouseClick = () => {
+
+  };
+
+  return (
+    <div onClick={handleMouseClick}>
+
+    </div>
+  );
+};
+
+// Define Diary component
+function Diary() {
   return (
     <div className="App">
-      <header className="StartingScreen">
+      <header className="App-header">
         <h1>This is my first diary.</h1>
-
         <header className="Blink">
-        <h6>Press any key to continue...</h6>
+          <h6>Press any key to continue...</h6>
         </header>
       </header>
     </div>
