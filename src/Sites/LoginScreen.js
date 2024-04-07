a00c2440131cdb809ca52a6998730694fad3e2de
Jere <jerekoivisto7@gmail.com>
2024-04-07 18:03:53 +0300

    Still have some work to do with login page, register page for example, half way done and then I get to work on the backend

--- src/Sites/LoginScreen.js
+++ src/Sites/LoginScreen.js
@@ -3,33 +3,41 @@ import "./styles.css";
 function LoginScreen() {
   return (
     <div class="wrapper">
-      <form action="">
-        <h1>Login</h1>
-        <div class="input-box">
-          <input type="text" placeholder="UserName" required></input>
-          <i class="bx bxs-user"></i>
-        </div>
-        <div class="input-box">
-          <input type="password" placeholder="Password" required></input>
-          <i class="bx bxs-lock-alt"></i>
-        </div>
-
-        <div class="remember-forgot">
-          <label>
-            <input type="checkbox"></input> Remember me
-          </label>
-          <a href="#">Forgot password?</a>
-        </div>
-
-        <button type="submit" class="btn">
-          Login
-        </button>
-        <div class="register-link">
-          <p>
-            Don't have an account? <a href="#">Register</a>
-          </p>
-        </div>
-      </form>
+      <div class="form-box login">
+        <h2>Login</h2>
+        <form action="#">
+          <div class="input-box">
+            <span class="icon">
+              <i class="bx bxs-user"></i>
+            </span>
+            <input type="email" placeholder="Email" required></input>
+          </div>
+          <div class="input-box">
+            <span class="icon">
+              <i class="bx bxs-lock-alt"></i>
+            </span>
+            <input type="password" placeholder="Password" required></input>
+          </div>
+          <div class="remember-forgot">
+            <label>
+              <input type="checkbox"></input>
+              Remember me
+            </label>
+            <a href="#">Forgot password?</a>
+          </div>
+          <button type="submit" class="btn">
+            Login
+          </button>
+          <div class="login-register">
+            <p>
+              Don't have an account?
+              <a href="#" class="register-link">
+                Register
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
