e05ae9637372bacf2896fd0d64da28f546563ca9
Jere <jerekoivisto7@gmail.com>
2024-04-14 18:02:01 +0300

    Register form and transition back to login form development

--- src/Sites/LoginScreen.js
+++ src/Sites/LoginScreen.js
@@ -8,7 +8,7 @@ function LoginScreen() {
         <form action="#">
           <div class="input-box">
             <span class="icon">
-              <i class="bx bxs-user"></i>
+              <i class="bx bxs-envelope"></i>
             </span>
             <input type="email" placeholder="Email" required></input>
           </div>
@@ -30,9 +30,49 @@ function LoginScreen() {
           </button>
           <div class="login-register">
             <p>
-              Don't have an account?
+              Don't have an account?{" "}
               <a href="#" class="register-link">
                 Register
+              </a>
+            </p>
+          </div>
+        </form>
+      </div>
+
+      <div class="form-box register">
+        <h2>Registration</h2>
+        <form action="#">
+          <div class="input-box">
+            <span class="icon">
+              <i class="bx bxs-user"></i>
+            </span>
+            <input type="email" placeholder="Username" required></input>
+          </div>
+          <div class="input-box">
+            <span class="icon">
+              <i class="bx bxs-envelope"></i>
+            </span>
+            <input type="email" placeholder="Email" required></input>
+          </div>
+          <div class="input-box">
+            <span class="icon">
+              <i class="bx bxs-lock-alt"></i>
+            </span>
+            <input type="password" placeholder="Password" required></input>
+          </div>
+          <div class="remember-forgot">
+            <label>
+              <input type="checkbox"></input>I agree to the terms & conditions
+            </label>
+          </div>
+          <button type="submit" class="btn">
+            Register
+          </button>
+          <div class="login-register">
+            <p>
+              Already have an account?{" "}
+              <a href="#" class="login-link">
+                Login
               </a>
             </p>
           </div>
