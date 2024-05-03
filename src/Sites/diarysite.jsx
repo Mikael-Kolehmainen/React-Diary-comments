8d15872a9c503b4c48e31f923d90d45dadf08310
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2024-05-03 21:30:11 +0300

    Added comments to diarysite.jsx

--- src/Sites/diarysite.jsx
+++ src/Sites/diarysite.jsx
@@ -1,19 +1,27 @@
-import './styles.css';
+/*
+  This file is not being used, but I'll still go through it.
 
-function Diary() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>This is my first diary.</h1>
-  
-          <header className="Blink">
+  Changes in this file, StartingScreen.jsx:
+    - From regular function syntax to arrow function syntax.
+    - Moved styles.css import to App.jsx.
+
+  Things that should be fixed:
+    - Inconsistent naming convention compared with other files, should be named
+    Diary.jsx for consistency.
+*/
+
+const Diary = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>This is my first diary.</h1>
+
+        <header className="Blink">
           <h6>Press any key to continue...</h6>
-          </header>
         </header>
-      </div>
+      </header>
+    </div>
+  );
+};
 
-      
-    );
-  }
-  
-  export default Diary;
+export default Diary;
